fba370dccfd0a08d33a870fc442afdfa8f8ce119
Tanvir Ahmmed <139853229+tanvirahmmed04@users.noreply.github.com>
2025-03-26 15:54:53 +0600

    Update index.js dynamic

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+/*
+#153A42
+#D72326
+#FFF
 
+
+*/
 //sign box reltaed
 let userbtn= document.querySelectorAll("#user");
 let signinupbox= document.querySelector(".signinup");
@@ -11,11 +17,58 @@ let backsignin= document.querySelector("#backsignin");
 
 
 
+//div controls
+// uiux control
+const uiuxbtn= document.querySelectorAll("#uiuxbtn");
+const uiuxpage= document.querySelector(".uiux-container")
+
+//graphics control
+const graphicsbtn= document.querySelectorAll("#graphicsbtn")
+const graphicspage= document.querySelector(".graphics-container");
+
+//3d design control
+const threedbtn= document.querySelectorAll("#threedbtn")
+const threedpage= document.querySelector(".three-d-conatainer");
+
+//brand promo control
+const brandpromobtn= document.querySelectorAll("#brandpromobtn");
+const brandpromopage= document.querySelector(".brand-promo-container");
+
+threedbtn.forEach(button=>{
+    button.onclick= function(act){
+        act.preventDefault();
+        threedpage.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest"  })
+    }
+})
+
+
+brandpromobtn.forEach(button=>{
+    button.onclick= function(act){
+        act.preventDefault();
+        brandpromopage.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest"  })
+    }
+})
+
+
+graphicsbtn.forEach(button=>{
+    button.onclick= function(act){
+        act.preventDefault();
+        graphicspage.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest"  })
+    }
+    
+})
+uiuxbtn.forEach(button=>{
+    button.onclick= function(act){
+        act.preventDefault();
+        uiuxpage.scrollIntoView({ behavior: "smooth" , block: "start", inline: "nearest" })
+    }
+})
 
 userbtn.forEach(button =>{
     button.onclick= function(){
         console.log("hello");
         signinupbox.style.display= "flex";
+        signinupbox.scrollIntoView({ behavior: "smooth" });
         signinbox.style.display="flex";
         signupbox.style.display="none";
     }
