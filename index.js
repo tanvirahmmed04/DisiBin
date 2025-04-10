d9016a0ae67f681669841cafb3c2355f35a266f3
Tanvir Ahmmed <139853229+tanvirahmmed04@users.noreply.github.com>
2025-04-11 05:24:15 +0600

    Update index.js april 11, 2025

--- index.js
+++ index.js
@@ -1,96 +1,104 @@
-/*
-#153A42
-#D72326
-#FFF
-
-
-*/
-//sign box reltaed
-let userbtn= document.querySelectorAll("#user");
-let signinupbox= document.querySelector(".signinup");
-let signinbox = document.querySelector(".signinbox");
-let signupbox = document.querySelector(".signupbox");
-let signupbtn= document.querySelector("#newsignup");
-let crosssignin= document.querySelector("#crosssignin");
-let crosssignup= document.querySelector("#crosssignup");
-let backsignin= document.querySelector("#backsignin");
-
-
-
-//div controls
-// uiux control
-const uiuxbtn= document.querySelectorAll("#uiuxbtn");
-const uiuxpage= document.querySelector(".uiux-container")
-
-//graphics control
-const graphicsbtn= document.querySelectorAll("#graphicsbtn")
-const graphicspage= document.querySelector(".graphics-container");
-
-//3d design control
-const threedbtn= document.querySelectorAll("#threedbtn")
-const threedpage= document.querySelector(".three-d-conatainer");
-
-//brand promo control
-const brandpromobtn= document.querySelectorAll("#brandpromobtn");
-const brandpromopage= document.querySelector(".brand-promo-container");
-
-threedbtn.forEach(button=>{
-    button.onclick= function(act){
-        act.preventDefault();
-        threedpage.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest"  })
-    }
-})
-
-
-brandpromobtn.forEach(button=>{
-    button.onclick= function(act){
-        act.preventDefault();
-        brandpromopage.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest"  })
-    }
-})
-
-
-graphicsbtn.forEach(button=>{
-    button.onclick= function(act){
-        act.preventDefault();
-        graphicspage.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest"  })
-    }
+// sidebaropen
+document.querySelector("#menubtn").onclick= function(){
+    let sidebar= document.querySelector(".sidebar");
+    sidebar.style.display= sidebar.style.display=== "flex"? "none": "flex";
+    console.log("menu btn was clicked");
     
-})
-uiuxbtn.forEach(button=>{
+}
+
+
+
+// userbox show
+const signinbox =document.querySelector(".signinbox");
+const gotosignin= document.querySelector("#gotosignin");
+const xsignin= document.querySelector("#xsignin");
+const xsignup= document.querySelector("#xsignup");
+const  gotosignup= document.querySelector("#gotosignup")
+const signupbox =document.querySelector(".signupbox");
+const userbox= document.querySelector(".userbox");
+const usericon= document.querySelectorAll("#usericon")
+const contentbox= document.querySelector(".contentbox");
+
+
+xsignup.onclick= function(){
+    userbox.style.display="none";
+    signupbox.style.display="none";
+    contentbox.style.display="flex";
+}
+xsignin.onclick= function(){
+    userbox.style.display="none";
+    signinbox.style.display="none";
+    contentbox.style.display="flex";
+}
+gotosignup.onclick= function(act){
+    act.preventDefault()
+    signupbox.style.display= "flex";
+    signinbox.style.display="none";
+}
+gotosignin.onclick= function(act){
+    act.preventDefault()
+    signupbox.style.display= "none";
+    signinbox.style.display="flex";
+}
+
+usericon.forEach((button)=>{
+    button.onclick= function(){
+        userbox.style.display= userbox.style.display=== "flex"? "none": "flex";
+        signinbox.style.display= signinbox.style.display=== "flex"? "none": "flex";
+       
+        contentbox.style.display= contentbox.style.display=== "none"? "flex": "none";
+
+
+    }
+});
+
+//userbox finish
+
+
+//navbar and sidebar actions
+const webdevbtn=document.querySelectorAll('#webdevbtn');
+const webdevbox= document.querySelector(".webdev");
+webdevbtn.forEach((button)=>{
     button.onclick= function(act){
         act.preventDefault();
-        uiuxpage.scrollIntoView({ behavior: "smooth" , block: "start", inline: "nearest" })
+        webdevbox.scrollIntoView({ behavior: 'smooth' })
+        
     }
 })
-
-userbtn.forEach(button =>{
-    button.onclick= function(){
+const veditbtn=document.querySelectorAll('#veditbtn');
+const veditbox= document.querySelector(".vedit");
+veditbtn.forEach((button)=>{
+    button.onclick= function(act){
+        act.preventDefault();
+        veditbox.scrollIntoView({ behavior: 'smooth' })
+        
+    }
+})
+const appdevbtn=document.querySelectorAll('#appdevbtn');
+const appdev= document.querySelector(".appdev");
+appdevbtn.forEach((button)=>{
+    button.onclick= function(act){
+        act.preventDefault();
+        appdev.scrollIntoView({ behavior: 'smooth' })
+        
+    }
+})
+const graphicsdevbtn=document.querySelectorAll('#graphicsdevbtn');
+const graphicsdevbox= document.querySelector(".graphicsdev");
+graphicsdevbtn.forEach((button)=>{
+    button.onclick= function(act){
+        act.preventDefault();
+        graphicsdevbox.scrollIntoView({ behavior: 'smooth' })
+        
+    }
+})
+const reviewbtn= document.querySelectorAll("#reviewbtn");
+reviewbtn.forEach((button)=>{
+    button.onclick= function(act){
+        act.preventDefault();
         console.log("hello");
-        signinupbox.style.display= "flex";
-        signinupbox.scrollIntoView({ behavior: "smooth" });
-        signinbox.style.display="flex";
-        signupbox.style.display="none";
+        
     }
 })
 
-signupbtn.onclick= function(){
-    signinbox.style.display="none";
-    signupbox.style.display="flex";
-}
-crosssignin.onclick= function(){
-    signinupbox.style.display= "none";
-    signinbox.style.display="none";
-    signupbox.style.display="none";
-}
-crosssignup.onclick= function(){
-    signupbox.style.display="none";
-    signinupbox.style.display="none";
-}
-backsignin.onclick= function(){
-    signinupbox.style.display= "flex";
-    signinbox.style.display="flex";
-    signupbox.style.display="none";
-}
-
-// sign box related end
+//finish
